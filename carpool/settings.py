72acccdb43059036ed6d7e7c697dd10644339935
Ifeanyijasper <ifeanyijasper11@gmail.com>
2021-06-18 10:54:29 +0100

    Modify image storage

--- carpool/settings.py
+++ carpool/settings.py
@@ -156,9 +156,9 @@ STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
-]
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'static')
+# ]
 
 MEDIA_URL = '/images/'
 
